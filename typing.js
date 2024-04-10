1e0776da96d2268d5473e13cb91aa8a6aaee6b7f
Juhen <141245635+juhen-ahmed@users.noreply.github.com>
2024-04-10 14:55:36 +0600

    Delete typing.js

--- typing.js
+++
@@ -1,7 +0,0 @@
-/*--------------------------- typing animation -----------------*/
-var typed = new Typed(".typing",{
-    strings:[" ","web Designer", "web Developer","Youtuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
